16a815da6015a5d328e22324ce11eabbf30b40c0
cllzkynk@gmail.com <cllzkynk@gmail.com>
2021-11-24 03:21:25 +0200

    _

--- src/level_3/Armstrong_Number.java
+++ src/level_3/Armstrong_Number.java
@@ -1,6 +1,5 @@
 package level_3;
 
-
 import java.util.Scanner;
 
 /*
@@ -15,41 +14,38 @@ public class Armstrong_Number {
 		 * kendisine eþitse bu sayýya Armstrong sayý denir.
 		 */
 
-		Scanner scan=new Scanner (System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Amstrong sayiya aday bir sayi gir");
-		int ady=scan.nextInt();
-		int depo=ady;
-		
-		String aday=Integer.toString(ady);
-		int uzunluk=aday.length()-1;
-				
-		int basamak=0;
-		int basamakDepo=1;
-		
-		int total=0;
-		
-		while (ady !=0) {
-			basamak=ady%10;
-			for (int i = uzunluk; i >=0; i--) {
-				basamakDepo*=basamak;
+		int ady = scan.nextInt();
+		int depo = ady;
+
+		String aday = Integer.toString(ady);
+		int uzunluk = aday.length() - 1;
+
+		int basamak = 0;
+
+		int total = 0;
+
+		while (ady != 0) {
+			int basamakDepo = 1;
+			basamak = ady % 10;
+			for (int i = uzunluk; i >= 0; i--) {
+				basamakDepo *= basamak;
 			}
-			
-			total=total+basamakDepo;
-			basamakDepo=1;
-		
-			ady/=10;
-			
+
+			total = total + basamakDepo;
+			// basamakDepo=1;
+
+			ady /= 10;
+
 		}
-		
-	
-		if (total==depo) {
-			System.out.println(depo+"  is an Amstrong number");
+
+		if (total == depo) {
+			System.out.println(depo + "  is an Amstrong number");
 		} else {
-			System.out.println(depo+"  is not an Amstrong number");
+			System.out.println(depo + "  is not an Amstrong number");
 		}
-		
-		
-		
+
 		scan.close();
 	}
 
